118f4b09ae0a13840e4870c49bac6e0371fed9e7
Richard Woessner <71599819+Richard-Woessner@users.noreply.github.com>
2021-05-13 15:52:48 -0400

    Overhaul
    Rebuilt application with functional frontend

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\Rick\PycharmProjects\djangoProject\venv\Scripts\python.exe
+#!C:\Users\Richa\PycharmProjects\liftLogger\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
